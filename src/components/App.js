fcacdd1310b795f690610aedfcd07405d0df9d0c
hemant4833 <73534621+hemant4833@users.noreply.github.com>
2022-10-18 03:32:17 +0530

    my button changes

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,24 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  
+  const shoot = () => {
+    document.getElementById("para").innerHTML = "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
+  }
   return (
+    
     <div id="main">
-      // Do not alter the main div
+    <p id="para"></p>
+    <button id="click" onClick={shoot}>on click</button>
+ 
     </div>
+   
+
   );
+
+
+ 
+
 }
 
 
